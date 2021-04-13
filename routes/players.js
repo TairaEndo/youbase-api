c4f0d05aab5800487dffcd0c68049596b2a5f807
Taira Endo <endo@nextbase.co.jp>
2021-04-13 22:46:23 +0900

    fix

--- routes/players.js
+++ routes/players.js
@@ -1,26 +1,57 @@
 var express = require('express');
 var router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
+const uri = process.env.DATABASE_URL || "mongodb+srv://taira:Guitenn1126@youbase-cluster.pigam.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+let client2
 
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+
+const main = async () => {
+  client2 = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+}
+
+main()
 
 router.get('/', function (req, res, next) {
-
-  //const start_ms = new Date().getTime();
-
-  const uri = "mongodb+srv://taira:Guitenn1126@youbase-cluster.pigam.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
-  const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-
   client.connect(err => {
-    //console.log(req.params.playerCode)
     client.db("central_batter_2020").collection("player_detail").find().toArray(function (err, docs) {
       res.json(docs)
       client.close();
     });
   })
 
-  // const elapsed_ms = new Date().getTime() - start_ms;
-  // console.log('処理時間aa：' + elapsed_ms, start_ms);
 });
+
+// router.get('/test', async (req, res, next) => {
+  
+//   let client2;
+//   console.log('hihi')
+//   try {
+//     console.log('hi!')
+//     client2 = await MongoClient.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+//     const db = client2.db('central_batter_2020')
+//     const collection = db.collection('player_detail')
+//     const data = await collection.find().toArray()
+//     res.json(data)
+//   }
+//   catch (err) {
+//     console.log(err);
+//     res.status(500).send();
+//   }
+// })
+
+router.get('/test2', async (req, res, next) => {
+  try {
+    const db = client2.db('central_batter_2020')
+    const collection = db.collection('player_detail')
+    const data = await collection.find().toArray()
+    res.json(data)
+  }
+  catch (err) {
+    console.log(err);
+    res.status(500).send();
+  }
+})
 
 
 
